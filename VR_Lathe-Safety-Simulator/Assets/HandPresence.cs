a0f14b83085bb864b3f383941664e1c3cf8100c9
Sarit Bas <basboss1818@gmail.com>
2021-04-17 21:38:20 +0700

    update

--- VR_Lathe-Safety-Simulator/Assets/HandPresence.cs
+++ VR_Lathe-Safety-Simulator/Assets/HandPresence.cs
@@ -5,6 +5,7 @@ using UnityEngine.XR;
 
 public class HandPresence : MonoBehaviour
 {
+    private InputDevice targetDevices;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,11 +18,28 @@ public class HandPresence : MonoBehaviour
             Debug.Log(item.name + item.characteristics);
         }
 
+        if(devices.Count > 0){
+            targetDevices = devices[0];
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        targetDevices.TryGetFeatureValue(CommonUsages.primaryButton,out bool primaryButtonValue);
+        if(primaryButtonValue){
+            Debug.Log("Pressing Primary Button");
+        }
 
+        targetDevices.TryGetFeatureValue(CommonUsages.trigger,out float triggerValue);
+        if(triggerValue > 0.1f){
+            Debug.Log("Trigger pressed" + triggerValue);
+        }
+
+        targetDevices.TryGetFeatureValue(CommonUsages.primary2DAxis,out Vector2 primary2DAxisValue);
+        if(primary2DAxisValue != Vector2.zero){
+            Debug.Log("Primary Touchpad " + primary2DAxisValue);
+        }        
     }
 }
