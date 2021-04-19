d95686a3743940e3d4da31f223db3d9828943d4c
Sarit Bas <basboss1818@gmail.com>
2021-04-20 00:07:06 +0700

    update animator hand

--- VR_Lathe-Safety-Simulator/Assets/HandPresence.cs
+++ VR_Lathe-Safety-Simulator/Assets/HandPresence.cs
@@ -1,45 +1,103 @@
-﻿ using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
 
 public class HandPresence : MonoBehaviour
 {
+
+    public bool showController = false;
+    public InputDeviceCharacteristics controllerCharacteristics;
+    public List<GameObject> controllerPrefabs;
+    public GameObject handModelPrefab;
     private InputDevice targetDevices;
+    private GameObject spawnedController;
+    private GameObject spawnedHandModel;
+    private Animator handAnimator;
     // Start is called before the first frame update
     void Start()
     {
         List<InputDevice> devices = new List<InputDevice>();
-        InputDeviceCharacteristics rightControllerCharacteristics = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
-        InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics, devices);
+
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
 
         foreach (var item in devices)
         {
             Debug.Log(item.name + item.characteristics);
         }
 
-        if(devices.Count > 0){
+        if (devices.Count > 0)
+        {
             targetDevices = devices[0];
+            GameObject prefab = controllerPrefabs.Find(controllerCharacteristics => controllerCharacteristics.name == targetDevices.name);
+            if (prefab)
+            {
+                spawnedController = Instantiate(prefab, transform);
+            }
+            else
+            {
+                Debug.LogError("Did not find corresponding controller model");
+                spawnedController = Instantiate(controllerPrefabs[0], transform);
+            }
+
+            spawnedHandModel = Instantiate(handModelPrefab, transform);
+            handAnimator = spawnedHandModel.GetComponent<Animator>(); 
         }
 
     }
 
+    void UpdateHandAnimation(){
+
+        if(targetDevices.TryGetFeatureValue(CommonUsages.trigger,out float triggerValue))
+        {
+            handAnimator.SetFloat("Trigger", triggerValue);
+
+        }else {
+            handAnimator.SetFloat("Trigger", 0);
+        }
+
+        if(targetDevices.TryGetFeatureValue(CommonUsages.grip,out float gripValue))
+        {
+            handAnimator.SetFloat("Grip", gripValue);
+
+        }else {
+            handAnimator.SetFloat("Grip", 0);
+        }
+
+    }
     // Update is called once per frame
     void Update()
     {
-        targetDevices.TryGetFeatureValue(CommonUsages.primaryButton,out bool primaryButtonValue);
-        if(primaryButtonValue){
-            Debug.Log("Pressing Primary Button");
-        }
+        // targetDevices.TryGetFeatureValue(CommonUsages.primaryButton,out bool primaryButtonValue);
+        // if(primaryButtonValue){
+        //     Debug.Log("Pressing Primary Button");
+        // }
 
-        targetDevices.TryGetFeatureValue(CommonUsages.trigger,out float triggerValue);
-        if(triggerValue > 0.1f){
-            Debug.Log("Trigger pressed" + triggerValue);
-        }
+        // targetDevices.TryGetFeatureValue(CommonUsages.trigger,out float triggerValue);
+        // if(triggerValue > 0.1f){
+        //     Debug.Log("Trigger pressed" + triggerValue);
+        // }
 
-        targetDevices.TryGetFeatureValue(CommonUsages.primary2DAxis,out Vector2 primary2DAxisValue);
-        if(primary2DAxisValue != Vector2.zero){
-            Debug.Log("Primary Touchpad " + primary2DAxisValue);
-        }        
+        // targetDevices.TryGetFeatureValue(CommonUsages.primary2DAxis,out Vector2 primary2DAxisValue);
+        // if(primary2DAxisValue != Vector2.zero){
+        //     Debug.Log("Primary Touchpad " + primary2DAxisValue);
+        // }
+
+        if (showController)
+        {
+            spawnedHandModel.SetActive(false);
+            spawnedController.SetActive(true);
+        }
+        else
+        {
+            if(spawnedHandModel != null){
+                spawnedHandModel.SetActive(true);
+            spawnedController.SetActive(false);
+            UpdateHandAnimation();
+            }else {
+                Debug.LogError("spawnedHandModel is null");
+            }
+            
+        }
     }
 }
