413d66e88364c408c9660edcea6538e79609e02d
Sarit Bas <basboss1818@gmail.com>
2021-04-26 12:02:17 +0700

    update

---
+++ VR_Lathe-Safety-Simulator/Assets/ButtonStrucer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonStrucer : MonoBehaviour
+{   
+
+    public bool isActivate;
+    public GameObject Button;
+    public Light buttonLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
