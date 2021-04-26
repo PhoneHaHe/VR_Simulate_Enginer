1f6a59170a4155cbd7983edf8cd892fb5e56fc42
Sarit Bas <basboss1818@gmail.com>
2021-04-26 21:35:36 +0700

    update

---
+++ VR_Lathe-Safety-Simulator/Assets/LocomotionController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class LocomotionController : MonoBehaviour
+{
+    
+    public XRController lefTeleportRay;
+    public XRController rightTeleportRay;
+    public InputHelpers.Button teleportActivationButton;
+    public float activationThreshold = 0.1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(lefTeleportRay){
+            lefTeleportRay.gameObject.SetActive(CheckIfActivated(lefTeleportRay));
+        }
+
+        if(rightTeleportRay){
+            rightTeleportRay.gameObject.SetActive(CheckIfActivated(rightTeleportRay));
+        }
+    }
+
+    public bool CheckIfActivated(XRController controller)
+    {
+        InputHelpers.IsPressed(controller.inputDevice, teleportActivationButton, out bool isActivated, activationThreshold);
+        return isActivated; 
+    }
+}
