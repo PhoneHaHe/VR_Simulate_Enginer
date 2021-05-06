0d13d255b74dc5dfc2e544c81a764dfc41f81bda
Sarit Bas <basboss1818@gmail.com>
2021-05-06 13:23:29 +0700

    Update

---
+++ VR_Lathe-Safety-Simulator/Assets/LatheController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LatheController : MonoBehaviour
+{
+    public GameObject lathePlatform;
+    public Vector3 position;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updatePositionOfPlatform();
+    }
+
+    void FixedUpdate(){
+        updatePositionOfPlatform();
+    }
+
+    public void MovetoLeft(){
+        // lathePlatform.transform.Translate();
+    }
+
+    public void MovetoRight(){
+        
+    }
+
+    public void updatePositionOfPlatform(){
+        position = lathePlatform.transform.position;
+    }
+}
