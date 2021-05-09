b14cc5bc7a0a14ac6de4d7d9291e7e48cea25be2
Sarit Bas <basboss1818@gmail.com>
2021-05-09 17:43:23 +0700

    Update

--- VR_Lathe-Safety-Simulator/Assets/WheelController.cs
+++ VR_Lathe-Safety-Simulator/Assets/WheelController.cs
@@ -6,24 +6,38 @@ public class WheelController : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject target;
-    public Quaternion ownAxis = Quaternion.identity;
+    public Vector3 currentPositionOfPlatform;
+    public float currentEulerAngles = 0;
+    public float MaxPosition = 3.429f;
+    public float MinPosition = 2.347f;
     void Start()
     {
-        ownAxis = transform.rotation;
+        currentPositionOfPlatform = target.transform.position;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (ownAxis.z > 0)
+        updateRotationOfValve();
+
+        if (currentEulerAngles < 0 && currentPositionOfPlatform.x >= MinPosition)//Left
         {
-            target.transform.position = target.transform.position + new Vector3(1, 0, 0) * Time.deltaTime;
+            target.transform.position = target.transform.position + new Vector3(currentEulerAngles / 100, 0, 0) * Time.deltaTime;
         }
-        else if (ownAxis.z < 0)
+        else if (currentEulerAngles > 0 && currentPositionOfPlatform.x <= MaxPosition)//Right
         {
-            target.transform.position = target.transform.position + -new Vector3(1, 0, 0) * Time.deltaTime;
-        }else {
-            target.transform.position = target.transform.position + -new Vector3(0, 0, 0) * Time.deltaTime;
+            target.transform.position = target.transform.position + new Vector3(currentEulerAngles / 100, 0, 0) * Time.deltaTime;
         }
+
     }
+
+    public void updateRotationOfValve(){
+
+        var turn = transform.localEulerAngles.z; 
+        turn = (turn > 180) ? turn - 360 : turn;
+        currentEulerAngles = turn;
+        currentPositionOfPlatform = target.transform.position;
+    }
+
+
 }
