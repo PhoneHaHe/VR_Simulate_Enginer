9035b037d697fc084dcc043d64b493478e92615c
Sarit Bas <basboss1818@gmail.com>
2021-05-14 10:28:45 +0700

    update safety and snap drill

---
+++ VR_Lathe-Safety-Simulator/Assets/SafetyButton.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SafetyButton : MonoBehaviour
+{
+    public bool isSafeActivate = false;
+    public bool buttonSafeHit = false;
+    private GameObject button;
+    private float buttonReturnToDistanceY = 0.0044f;
+    private float buttonReturnToDistanceZ = 0.0007f;
+    private float buttonReturnSpeed = 0.0001f;
+    private float buttonOriginY;
+    private float buttonOriginZ;
+
+
+    //delay for hit
+    private float buttonCanHitAgainTime = 0.5f;
+    private float canHitAgain;
+
+    //ref to Main Button
+    private ButtonStrucer buttonMain;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = transform.GetChild(0).gameObject;
+        buttonOriginY = button.transform.position.y;
+        buttonOriginZ = button.transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate() {
+        
+    }
+
+    private void TryAccessToButton()
+    {
+        if (buttonSafeHit == true)
+        {
+
+            buttonSafeHit = false;
+
+            isSafeActivate = !isSafeActivate;
+
+            button.transform.position = new Vector3(button.transform.position.x, button.transform.position.y - buttonReturnToDistanceY, button.transform.position.z - buttonReturnToDistanceZ);
+
+            safetyActivate();
+
+        }
+
+        if (button.transform.position.y < buttonOriginY && button.transform.position.z < buttonOriginZ)
+        {
+            button.transform.position += new Vector3(0, buttonReturnSpeed, buttonReturnSpeed);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        Debug.Log("Tracking Something ");
+
+        if (other.CompareTag("HandPlayer") && canHitAgain < Time.time)
+        {
+            Debug.Log("HandPlayer Tracking");
+            canHitAgain = Time.time + buttonCanHitAgainTime;
+            buttonSafeHit = true;
+        }
+    }
+
+    void safetyActivate(){
+        Debug.Log("Safe Action isCall");
+        buttonMain.isActivate = false;
+    }
+}
