9aaac209ba95c610a3c42104f6fc8d997e48a63a
Sarit Bas <basboss1818@gmail.com>
2021-05-15 11:13:21 +0700

    Update add InputManager Script

---
+++ VR_Lathe-Safety-Simulator/Assets/InputManager.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.Events;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField]
+    public XRNode xRNode;
+
+    private List<InputDevice> devices = new List<InputDevice>();
+
+    private InputDevice device;
+
+    //to avoid repeat readings
+    private bool triggerIsPressed;
+    private bool primaryButtonIsPressed;
+    private bool primary2DAxisIsChosen;
+    private Vector2 primary2DAxisValue = Vector2.zero;
+    private Vector2 prevPrimary2DAxisValue;
+    private bool gripIsPressed;
+
+    void GetDevice()
+    {
+        InputDevices.GetDevicesAtXRNode(xRNode, devices);
+    }
+
+    void OnEnable()
+    {
+        if (!device.isValid)
+        {
+            GetDevice();
+        }
+    }
+
+    void Update()
+    {
+        if (!device.isValid)
+        {
+            GetDevice();
+        }
+
+        // capturing trigger button press and release    
+        bool triggerButtonValue = false;
+        if (device.TryGetFeatureValue(CommonUsages.triggerButton, out triggerButtonValue) && triggerButtonValue && !triggerIsPressed)
+        {
+            triggerIsPressed = true;
+            Debug.Log($"TriggerButton activated {triggerButtonValue} on {xRNode}");
+        }
+        else if (!triggerButtonValue && triggerIsPressed)
+        {
+            triggerIsPressed = false;
+            Debug.Log($"TriggerButton deactivated {triggerButtonValue} on {xRNode}");
+        }
+
+        // capturing primary button press and release
+        bool primaryButtonValue = false;
+        InputFeatureUsage<bool> primaryButtonUsage = CommonUsages.primaryButton;
+
+        if (device.TryGetFeatureValue(primaryButtonUsage, out primaryButtonValue) && primaryButtonValue && !primaryButtonIsPressed)
+        {
+            primaryButtonIsPressed = true;
+            Debug.Log($"PrimaryButton activated {primaryButtonValue} on {xRNode}");
+        }
+        else if (!primaryButtonValue && primaryButtonIsPressed)
+        {
+            primaryButtonIsPressed = false;
+            Debug.Log($"PrimaryButton deactivated {primaryButtonValue} on {xRNode}");
+        }
+
+        // capturing primary 2D Axis changes and release
+        InputFeatureUsage<Vector2> primary2DAxisUsage = CommonUsages.primary2DAxis;
+        // make sure the value is not zero and that it has changed
+        if (primary2DAxisValue != prevPrimary2DAxisValue)
+        {
+            primary2DAxisIsChosen = false;
+            //Debug.Log($"CHANGED and prev value is {prevPrimary2DAxisValue} and the new value is {primary2DAxisValue}");
+        }
+        // was for checking to see if the axis values were reading as changed properly
+        /* else
+        {
+            Debug.Log($"Nope, prev value is {prevPrimary2DAxisValue} and the new value is {primary2DAxisValue}");
+        } */
+        if (device.TryGetFeatureValue(primary2DAxisUsage, out primary2DAxisValue) && primary2DAxisValue != Vector2.zero && !primary2DAxisIsChosen)
+        {
+            prevPrimary2DAxisValue = primary2DAxisValue;
+            primary2DAxisIsChosen = true;  
+            Debug.Log($"Primary2DAxis value activated {primary2DAxisValue} on {xRNode}");
+        }
+        else if (primary2DAxisValue == Vector2.zero && primary2DAxisIsChosen)
+        {
+            prevPrimary2DAxisValue = primary2DAxisValue;
+            primary2DAxisIsChosen = false;
+            Debug.Log($"Primary2DAxis deactivated {primary2DAxisValue} on {xRNode}");
+        }
+
+        // capturing grip value
+        float gripValue;
+        InputFeatureUsage<float> gripUsage = CommonUsages.grip;
+
+        if (device.TryGetFeatureValue(gripUsage, out gripValue) && gripValue > 0 && !gripIsPressed)
+        {
+            gripIsPressed = true;
+            Debug.Log($"Grip value {gripValue} activated on {xRNode}");
+        }
+        else if (gripValue == 0 && gripIsPressed)
+        {
+            gripIsPressed = false;
+            Debug.Log($"Grip value {gripValue} deactivated on {xRNode}");
+        }
+    }
+}
+
--- VR_Lathe-Safety-Simulator/Assets/Wheeling.cs
+++ VR_Lathe-Safety-Simulator/Assets/Wheeling.cs
@@ -3,45 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
 
-public class Wheeling : MonoBehaviour
+public class Wheeling : InputManager
 {
-   /* public InputDeviceCharacteristics controllerCharacteristics;
-    private InputDevice targetDevices;
-    // Start is called before the first frame update
-    void Start()
-    {
-        List<InputDevice> devices = new List<InputDevice>();
+    // public InputDeviceCharacteristics controllerCharacteristics;
+    // private InputDevice targetDevices;
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+    //     List<InputDevice> devices = new List<InputDevice>();
 
-        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
-    }
+    //     InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
+    // }
 
-    // Update is called once per frame
-    void Update()
-    {
-        TryInitializeController();
-    }
-    void FixedUpdate() {
-        TryInitializeController();
-    }
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     TryInitializeController();
+    // }
+    // void FixedUpdate() {
+    //     TryInitializeController();
+    // }
 
-    void TryInitializeController() {
+    // void TryInitializeController() {
 
-        targetDevices.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue);
-        if (primaryButtonValue)
-        {
-            Debug.Log("Pressing Primary Button");
-        }
+    //     targetDevices.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue);
+    //     if (primaryButtonValue)
+    //     {
+    //         Debug.Log("Pressing Primary Button");
+    //     }
 
-        targetDevices.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue);
-        if (triggerValue > 0.1f)
-        {
-            Debug.Log("Trigger pressed" + triggerValue);
-        }
+    //     targetDevices.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue);
+    //     if (triggerValue > 0.1f)
+    //     {
+    //         Debug.Log("Trigger pressed" + triggerValue);
+    //     }
 
-        targetDevices.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DAxisValue);
-        if (primary2DAxisValue != Vector2.zero)
-        {
-            Debug.Log("Primary Touchpad " + primary2DAxisValue);
-        }
-    }*/
+    //     targetDevices.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DAxisValue);
+    //     if (primary2DAxisValue != Vector2.zero)
+    //     {
+    //         Debug.Log("Primary Touchpad " + primary2DAxisValue);
+    //     }
+    // }
+
+    
 }
