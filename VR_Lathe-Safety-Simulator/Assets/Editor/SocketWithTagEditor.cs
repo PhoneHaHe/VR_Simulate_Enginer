de4d211ac958b51570bb9e1fdf39ade4e5103382
Sarit Bas <basboss1818@gmail.com>
2021-05-16 09:37:34 +0700

    Update recheck

---
+++ VR_Lathe-Safety-Simulator/Assets/Editor/SocketWithTagEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.XR.Interaction.Toolkit;
+
+public class SocketWithTagEditor : XRSocketInteractorEditor
+{
+    private SerializedProperty targetTag = null;
+
+    protected override void OnEnable(){
+        
+        base.OnEnable();
+        targetTag = SerializedObject.FindProperty("targetTag");
+    }
+    
+}
---
+++ VR_Lathe-Safety-Simulator/Assets/Scripts/SnapWithTag.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine.XR.Interaction.Toolkit;
+
+public class SnapWithTag : XRSocketInteractor
+{
+
+    public string targetTag = string.Empty;
+
+    public override bool CanHover(XRBaseInteractable interactable)
+    {
+        return base.CanHover(interactable) && MatchUsingTag(interactable);
+    }
+
+    public override bool CanSelect(XRBaseInteractable interactable)
+    {
+        return base.CanSelect(interactable) && MatchUsingTag(interactable);
+    }
+
+    private bool MatchUsingTag(XRBaseInteractable interactable)
+    {
+        return interactable.CompareTag(targetTag);
+    }
+}
