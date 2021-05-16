a120ce545038de26fbf85200ddac679ec8c8ee0e
Sarit Bas <basboss1818@gmail.com>
2021-05-16 09:53:34 +0700

    update sound guard

---
+++ VR_Lathe-Safety-Simulator/Assets/PlayedSound.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayedSound : MonoBehaviour
+
+{
+
+    public AudioSource Guardclose;
+    
+
+    void PlaySound()
+    {
+        Guardclose.Play();
+    }
+}
