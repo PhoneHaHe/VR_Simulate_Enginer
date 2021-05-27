cc5423bd40712535025391b5fd2caa213846c4a4
Sarit Bas <basboss1818@gmail.com>
2021-05-27 17:25:43 +0700

    Update

---
+++ VR_Lathe-Safety-Simulator/Assets/CheckStatusTools.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckStatusTools : MonoBehaviour
+{
+
+    [SerializeField] private List<Transform> _StatusBarOfTools;
+    [SerializeField] private Material _ToThisMaterial;
+
+    public void UpdateStatusBar(int other) {
+
+        if (_StatusBarOfTools != null) {
+
+            var _StatusBar = _StatusBarOfTools[other].transform.GetComponent<Renderer>();
+
+            _StatusBar.material = _ToThisMaterial;
+        
+        }
+    }
+}
